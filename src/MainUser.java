359e4ff6dca506bd7133b9818c9c5998630804e0
Tatiana <in_sdo@mail.ru>
2023-03-27 14:29:29 +0300

    User

---
+++ src/MainUser.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.net.Socket;
+
+
+public class MainUser {
+    public static void main(String[] args) {
+        String host = "127.0.0.1";
+
+        int port = 8080;
+        try (Socket clientSocket = new Socket(host,port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+        ) {
+            out.println("Tatyana");
+            String resp = in.readLine();
+            System.out.println(resp);
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
